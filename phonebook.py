c6e4f9c314bc9af0706f54f9ed00c42947c4e6ad
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-05-10 16:19:50 -0400

    added edge case functionality

--- phonebook.py
+++ phonebook.py
@@ -12,7 +12,13 @@ running = True
 phonebook = {}
 
 while running == True:
-    user_input = int(input("What do you want to do (1-5)? "))
+    
+    # Handlie invalid input
+    try:
+        user_input = int(input("What do you want to do (1-5)? "))
+    except:
+        print("Invalid input. Please enter a number between 1 and 5.")
+        continue
     
     # Look up entry in phonebook
     if user_input == 1:
@@ -25,7 +31,7 @@ while running == True:
             print("No entries found.")
     
     # Add entry to phonebook
-    if user_input == 2:
+    elif user_input == 2:
         name = input("Name: ")
         phone_number = input("Phone Number: ")
         phonebook[name] = {
@@ -34,16 +40,20 @@ while running == True:
         print("Entry stored for %s." % (name))
     
     # Delete an entry from phonebook
-    if user_input == 3:
+    elif user_input == 3:
         name = input("Name: ")
         if name in phonebook:
             del phonebook[name]
         print("Deleted entry for %s." % (name))
     
     # List all entries
-    if user_input == 4:
+    elif user_input == 4:
         for entry in phonebook:
             print("Found entry for %s: %s" % (entry, phonebook[entry]["phone_number"]))
     # Quit
-    if user_input == 5:
+    elif user_input == 5:
         running = False
+        
+    # Handle invalid input
+    else:
+        print("Invalid input. Please enter a number between 1 and 5.")
